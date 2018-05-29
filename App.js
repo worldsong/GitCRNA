b24ca275b5902a338bbac5253c5e0086e00c7b0d
worldsong <2060884089@qq.com>
2018-05-29 21:51:14 +0800

    修正导航标签title的色差。

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ export default class App extends React.Component {
               <TabNavigator.Item
                   selected={this.state.selectedTab === 'popular'}
                   title="最热"
+                  selectedTitleStyle={{color:'#63B8FF' }}
                   renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_popular.png')} />}
                   renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_popular.png')} />}
                   onPress={() => this.setState({ selectedTab: 'popular' })}>
@@ -24,6 +25,7 @@ export default class App extends React.Component {
               <TabNavigator.Item
                   selected={this.state.selectedTab === 'trending'}
                   title="趋势"
+                  selectedTitleStyle={{color:'#63B8FF' }}
                   renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_trending.png')} />}
                   renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_trending.png')} />}
                   onPress={() => this.setState({ selectedTab: 'trending' })}>
@@ -32,6 +34,7 @@ export default class App extends React.Component {
               <TabNavigator.Item
                   selected={this.state.selectedTab === 'favorite'}
                   title="收藏"
+                  selectedTitleStyle={{color:'#63B8FF' }}
                   renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_favorite.png')} />}
                   renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_favorite.png')} />}
                   onPress={() => this.setState({ selectedTab: 'favorite' })}>
@@ -40,6 +43,7 @@ export default class App extends React.Component {
               <TabNavigator.Item
                   selected={this.state.selectedTab === 'my'}
                   title="我的"
+                  selectedTitleStyle={{color:'#63B8FF' }}
                   renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_my.png')} />}
                   renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_my.png')} />}
                   onPress={() => this.setState({ selectedTab: 'my' })}>
