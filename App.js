8ac22d0ae620b02b2893e022910146ce33a845d0
worldsong <2060884089@qq.com>
2018-05-29 21:31:04 +0800

    引入react-native-tab-navigator，实现底部导航。

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedTab: 'popular'
+        }
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <TabNavigator>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'popular'}
+                  title="最热"
+                  renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_popular.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_popular.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'popular' })}>
+                  <View style={{backgroundColor: 'red', flex:1}}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'trending'}
+                  title="趋势"
+                  renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_trending.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_trending.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'trending' })}>
+                  <View style={{backgroundColor: 'yellow', flex:1}}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'favorite'}
+                  title="收藏"
+                  renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_favorite.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_favorite.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'favorite' })}>
+                  <View style={{backgroundColor: 'green', flex:1}}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'my'}
+                  title="我的"
+                  renderIcon={() => <Image style={styles.icon} source={require('./res/images/ic_my.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.icon,{tintColor:'#63B8FF'}]} source={require('./res/images/ic_my.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'my' })}>
+                  <View style={{backgroundColor: 'blue', flex:1}}></View>
+              </TabNavigator.Item>
+          </TabNavigator>
       </View>
     );
   }
@@ -16,8 +54,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+    icon: {
+        width:26,
+        height:26
+    }
 });
