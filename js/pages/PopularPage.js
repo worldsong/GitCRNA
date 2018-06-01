dc6917fe8abff9245e3f28d852a96e449f79d42b
worldsong <2060884089@qq.com>
2018-06-01 20:31:58 +0800

    使用FlatList展示列表数据。

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -2,16 +2,25 @@
  * Created by Song on 2018/5/29.
  */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import NavigationBar from '../components/NavigationBar'
 
 //包含两块内容，状态栏（静），滚动视图（动）
 export default class PopularPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            dataSource: ['时间的导师', '干净的字迹', '小二']
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
                 <NavigationBar />
-                <View style={{backgroundColor:'red',flex:1}}></View>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({item}) => <Text>{item}</Text>}
+                />
             </View>
         );
     }
