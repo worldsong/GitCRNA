5dc195348bf75355c1927c6a31745b9761eb54f6
worldsong <2060884089@qq.com>
2018-06-01 20:51:18 +0800

    滚动列表react-native-scrollable-tab-view。

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -4,28 +4,45 @@
 import React from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
 import NavigationBar from '../components/NavigationBar'
+import ScrollableTabView from 'react-native-scrollable-tab-view'
 
 //包含两块内容，状态栏（静），滚动视图（动）
 export default class PopularPage extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <NavigationBar />
+                <ScrollableTabView
+                    tabBarBackgroundColor="#63B8FF"
+                    tabBarActiveTextColor="#FFF"
+                    tabBarInactiveTextColor="#F5FFFA"
+                    tabBarUnderlineStyle={{backgroundColor:"#E7E7E7",height:2}}>
+                    <PopularTab tabLabel='IOS' />
+                    <PopularTab tabLabel='Android'/>
+                    <Text tabLabel='Java'>JAVA</Text>
+                    <PopularTab tabLabel='Javascript'/>
+                </ScrollableTabView>
+            </View>
+        );
+    }
+}
+
+class PopularTab extends React.Component {
     constructor(props){
         super(props);
         this.state= {
             dataSource: [{key:'时间的导师'}, {key:'干净的字迹'}, {key:'小二'}]
         }
     }
-    render() {
+    render(){
         return (
-            <View style={styles.container}>
-                <NavigationBar />
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={({item}) => <Text>{item.key}</Text>}
-                />
-            </View>
-        );
+            <FlatList
+                data={this.state.dataSource}
+                renderItem={({item}) => <Text>{item.key}</Text>}
+            />
+        )
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1
