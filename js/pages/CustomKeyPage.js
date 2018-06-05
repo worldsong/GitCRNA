098239294a45f87161f1f0507a96d4da026e85ce
worldsong <2060884089@qq.com>
2018-06-05 22:14:55 +0800

    自定义分类组件实现。

--- js/pages/CustomKeyPage.js
+++ js/pages/CustomKeyPage.js
@@ -4,8 +4,18 @@
 import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import NavigationBar from '../components/NavigationBar'
+import CheckBox from "react-native-check-box";
+
+
+const popular_def_lans = require('../../res/data/popular_def_lans.json');
 
 export default class CustomKeyPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : popular_def_lans
+        };
+    }
     doBack = () => {
         this.props.navigator.pop();
     }
@@ -32,6 +42,43 @@ export default class CustomKeyPage extends React.Component {
             </TouchableOpacity>
         </View>;
     }
+    handleClick = (item)=>{
+        item.checked = !item.checked;
+        this.setState({isModified:true});//修改了
+    }
+    renderCheckBox = (item)=>{
+        console.log(item.name+','+item.checked);
+        return <CheckBox
+            style={{flex:1, padding:10}}
+            onClick={()=>this.handleClick(item)}
+            leftText={item.name}
+            isChecked={item.checked}
+            unCheckedImage={<Image source={require('../../res/images/ic_check_box_outline_blank.png')} style={styles.checkbox}/>}
+            checkedImage={<Image source={require('../../res/images/ic_check_box.png')} style={styles.checkbox}/>}
+        />
+    }
+
+    renderViews = ()=>{
+        let len = this.state.data.length;
+        let views = [];  //要绘制的所有多选框，装入views数组
+        for(let i = 0, j = len - 2; i < j; i+=2){
+            views.push((
+                <View key={`view_${i}`} style={{flexDirection:'row'}}>
+                    {this.renderCheckBox(this.state.data[i])}
+                    {this.renderCheckBox(this.state.data[i+1])}
+                </View>
+            ));
+        }
+        //偶数个，剩下最后两个多选框
+        //奇数个，剩下最后一个多选框
+        views.push(
+            <View key={`view_${len-1}`} style={{flexDirection:'row'}}>
+                {len % 2 === 0 ? this.renderCheckBox(this.state.data[len-2]) : <View style={{flex:1, padding:10}}></View>}
+                {this.renderCheckBox(this.state.data[len-1])}
+            </View>
+        );
+        return views;
+    }
     render() {
         return (
             <View style={styles.container}>
@@ -40,7 +87,9 @@ export default class CustomKeyPage extends React.Component {
                     rightButton={this.getNavRightBtn()}
                     leftButton={this.getNavLeftBtn()}
                 />
-                <Text>RN开发组件模板</Text>
+                <View style={{flexDirection:'column'}}>
+                    {this.renderViews()}
+                </View>
             </View>
         );
     }
@@ -50,4 +99,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
+    checkbox:{
+        tintColor: '#63B8FF'
+    }
 });
