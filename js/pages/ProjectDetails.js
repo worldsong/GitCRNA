46f158e7ea4b042aa5f64ddef68f6ee5ab23169e
worldsong <2060884089@qq.com>
2018-06-08 20:44:29 +0800

    WebView的onNavigationStateChange显示自身的控制权限范围。

--- js/pages/ProjectDetails.js
+++ js/pages/ProjectDetails.js
@@ -2,15 +2,45 @@
  * Created by Song on 2018/6/8.
  */
 import React from 'react';
-import {StyleSheet, Text, View, WebView} from 'react-native';
+import {StyleSheet, Text, View, WebView, TouchableOpacity, Image} from 'react-native';
+import NavigationBar from '../components/NavigationBar';
 
 export default class ProjectDetails extends React.Component {
+    constructor(props){
+        super(props);
+        this.state= {canGoBack: false}
+    }
+    handleBack = () => {
+        if(this.state.canGoBack){
+            this.refs.webview.goBack();
+        }else{
+            this.props.navigator.pop();
+        }
+    }
+    getNavLeftBtn = ()=>{
+        return <View style={{flexDirection:'row',alignItems:'center'}}>
+            <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={this.handleBack}>
+                <Image source={require('../../res/images/ic_arrow_back_white_36pt.png')} style={{width:24,height:24}}/>
+            </TouchableOpacity>
+        </View>;
+    }
+    handleNavStateChange = (s) => {
+        this.setState({canGoBack: s.canGoBack})
+    }
     render() {
         return (
             <View style={styles.container}>
+                <NavigationBar
+                    title="项目详情页"
+                    leftButton={this.getNavLeftBtn()}
+                />
                 <WebView
+                    ref="webview"
                     startInLoadingState={true}
                     source={{uri:'http://songfens.club'}}
+                    onNavigationStateChange={this.handleNavStateChange}
                 />
             </View>
         );
